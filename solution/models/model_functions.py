23ad4886f7b5db80e55b794dfac7daf9c78e175c
Carla Sequero <carlasequero@gmail.com>
2026-08-17 23:03:23 +0200

    modified code from dense model to use functions from a py

---
+++ solution/models/model_functions.py
@@ -0,0 +1,83 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+def plot_training_metrics(model_history):
+    epochs = range(1, len(model_history["loss"]) + 1)
+
+    metrics = ["loss", "mae", "mse"]
+
+    for metric in metrics:
+        plt.figure(figsize=(9, 4))
+        plt.plot(epochs, model_history[metric], label=f"Métrica {metric} de entrenamiento")
+        plt.plot(epochs, model_history[f"val_{metric}"], label=f"Métrica {metric} de validación")
+
+        plt.xlabel("Época")
+        plt.ylabel(metric.upper())
+        plt.title(f"Evolución de {metric}")
+        plt.legend()
+        plt.grid(alpha=0.3)
+        plt.show()
+
+
+def plot_prediction_example(
+    i,
+    sdss_wavelength,
+    gaia_wavelength,
+    y_test,
+    y_pred,
+    y_id_test,
+    X_test,
+    X_id_test
+):
+    plt.figure(figsize=(9, 4))
+    plt.plot(sdss_wavelength, y_test[i], label="SDSS real", alpha = 0.5)
+    plt.plot(sdss_wavelength, y_pred[i], label="SDSS predicho", alpha = 0.9)
+    plt.plot(gaia_wavelength, X_test[i], label="Gaia entrada")
+    plt.xlabel("Longitud de onda [Å]")
+    plt.ylabel("Flujo")
+    plt.title(f"Gaia id: {X_id_test[i]} | SDSS id: {y_id_test[i]}")
+    plt.grid(alpha=0.2)
+    plt.legend()
+    plt.show()
+
+
+def plot_worst_best_predictions(
+    y_test,
+    y_pred,
+    X_test,
+    X_id_test,
+    sdss_wavelength,
+    gaia_wavelength,
+    n_worst=10
+):
+
+    mae_per_object = np.mean(np.abs(y_test - y_pred), axis=1)
+    mse_per_object = np.mean((y_test - y_pred) ** 2, axis=1)
+
+    worst_idx = np.argsort(mse_per_object)[-n_worst:]
+
+    for i in worst_idx:
+        plt.figure(figsize=(10, 4))
+        plt.plot(sdss_wavelength, y_test[i], label="SDSS real", alpha = 0.5)
+        plt.plot(sdss_wavelength, y_pred[i], label="SDSS predicho", alpha = 0.9)
+        plt.plot(gaia_wavelength, X_test[i], label="Gaia")
+        plt.xlabel("Longitud de onda [Å]")
+        plt.ylabel("Flujo")
+        plt.title(f"Peor objeto test {i} | id Gaia: {X_id_test[i]} | MSE={mse_per_object[i]:.3f} | MAE={mae_per_object[i]:.3f}")
+        plt.grid(alpha=0.2)
+        plt.legend()
+        plt.show()
+
+    best_idx = np.argsort(mse_per_object)[:10]
+
+    for i in best_idx:
+        plt.figure(figsize=(10, 4))
+        plt.plot(sdss_wavelength, y_test[i], label="SDSS real", alpha = 0.5)
+        plt.plot(sdss_wavelength, y_pred[i], label="SDSS predicho", alpha = 0.9)
+        plt.plot(gaia_wavelength, X_test[i], label="Gaia")
+        plt.xlabel("Longitud de onda [Å]")
+        plt.ylabel("Flujo")
+        plt.title(f"Mejor objeto test {i} | id Gaia: {X_id_test[i]} | MSE={mse_per_object[i]:.3f} | MAE={mae_per_object[i]:.3f}")
+        plt.grid(alpha=0.2)
+        plt.legend()
+        plt.show()
